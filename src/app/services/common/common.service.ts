b726487bbb47a2f6dd141a4fe9c0b5f52af440a8
Eze32 <ezeq.quevedo@gmail.com>
2020-10-17 08:33:59 -0300

    Se modifica la url del servicioy  metodo de envio por post.

--- src/app/services/common/common.service.ts
+++ src/app/services/common/common.service.ts
@@ -8,14 +8,14 @@ import { User } from 'src/app/components/commons/models/User';
 export class CommonService {
 
    // url = 'https://jsonplaceholder.typicode.com/posts';
-   url = 'http://localhost:8080';
+   url = 'https://stage-blue-mobius-mind-api.herokuapp.com';
 
   constructor(public http: HttpClient, public httpHandler: HttpHandler) {
     this.http = new HttpClient(httpHandler);
    }
 
   public login(user: User) {
-    const urlService = this.url + '/login';
+    const urlService = this.url + '/security/signin';
     let hash: any;
     const mensaje = {
       email: null,
@@ -24,7 +24,7 @@ export class CommonService {
     mensaje.email = user.email;
     mensaje.password = user.password;
     hash = 'Bearer: '.concat(btoa('mensaje.email:mensaje.password'));
-    const respuesta = this.http.post(urlService, hash);
+    const respuesta = this.http.post(urlService, mensaje, hash);
     return respuesta;
   }
 
