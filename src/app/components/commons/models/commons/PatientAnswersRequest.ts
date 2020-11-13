f4ee5004044f301873151f88aa9c6bd8ae951bd4
DESKTOP-QR0H4IC\dgayo <damiangayoso@gmail.com>
2020-11-13 17:27:05 -0300

    se corrigio una linea que pedia cambios

--- src/app/components/commons/models/commons/PatientAnswersRequest.ts
+++ src/app/components/commons/models/commons/PatientAnswersRequest.ts
@@ -1,9 +1,5 @@
 export class PatientAnswersRequest<T> {
     id: number;
     isCorrect: boolean;
-<<<<<<< HEAD
-=======
-    answer: any;
->>>>>>> 640225bae0387b1c28c924cb2a061be9663ff597
     answer: T;
 }
