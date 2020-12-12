4e0874fd12da4f7a3d9e2ee99d95876351272101
Eze32 <ezeq.quevedo@gmail.com>
2020-12-12 19:46:38 -0300

    Se agraga para que boton este habilitado o deshabilitado.

--- src/app/components/commons/generico/formulario/formulario.component.ts
+++ src/app/components/commons/generico/formulario/formulario.component.ts
@@ -38,6 +38,7 @@ export class FormularioComponent implements OnInit {
   @Input() UrlProxPantalla = '/test/introduccion';
   @Input() NombreBotonSubmit = '';
   @Input() SoloForm = true;
+  @Input() btnDeshabilitado = false;
 
   // SALIDAS
   @Output() Cargando = new EventEmitter<boolean>();
